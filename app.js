c61eb6f3f92dc880ed9f3c8468eabcf19c2352cb
Riki <rikikarl8@gmail.com>
2022-02-01 17:39:27 +0800

    Finished Back-end

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-let songTitle = document.getElementById;
+document.getElementById("search-btn").addEventListener("click", Fetch);
 
-fetch("https://api.lyrics.ovh/v1/Playboi Carti/Stop Breathing")
-  .then(function (response) {
-    console.log("Helloooo   ");
-    // if statement to catch any errors aside from 404
-    if (response.status != 200) {
-      console.log(
-        "Looks like there was a problem. Status Code: " + response.status
-      );
-      return;
-    }
+function Fetch() {
+  // fetch("https://api.lyrics.ovh/v1/Playboi Carti/Meh/");
+  let songTitle = document.getElementById("song-user").value;
+  let artistName = document.getElementById("artist-user").value;
+  document.getElementById("result-title").innerHTML = songTitle;
+  fetch("https://api.lyrics.ovh/v1/" + artistName + "/" + songTitle)
+    .then(function (response) {
+      console.log("Helloooo");
 
-    response.json().then(function (data) {
-      console.log("Successful!");
-      console.log(data);
-      //   data1 = data.url;
-      //   jsonData = JSON.parse(data1.content);
-      const lyrics = data.lyrics.replace(new RegExp("\n", "g"), "<br>");
-      document.getElementById("lyrics-display").innerHTML = lyrics;
+      // if statement to catch any errors aside from 404
+      if (response.status != 200) {
+        console.log(
+          "Looks like there was a problem. Status Code: " + response.status
+        );
+        return;
+      }
+
+      response.json().then(function (data) {
+        console.log("Successful!");
+        console.log(data);
+        const lyrics = data.lyrics.replace(new RegExp("\n", "g"), "<br>");
+        document.getElementById("lyrics-display").innerHTML = lyrics;
+      });
+    })
+    .catch(function (err) {
+      console.log(err + "404");
     });
-  })
-  .catch(function (err) {
-    console.log(err + "404");
-  });
+}
